b5c6d907facbedf29be7df49f447381f069df620
BappySheikh1 <sheikhbappy91@gmail.com>
2024-05-16 12:21:04 +0600

    function done

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-// normal function
+// normal function ans declaration
 
 // function add(num1, num2){
 //   return num1 + num2
@@ -17,3 +17,42 @@
 // const result = addNumbers(10,22)
 // console.log(result)
 
+// const addNumbers =(num1, num2) => num1 + num2
+// const result = addNumbers(2,4)
+// console.log(result)
+
+
+
+// function expression
+// const multiply = function (num1, num2){
+//  return num1 + num2
+// }
+
+// console.log(multiply(2,3))
+
+//  Arrow function e declaration tai kora hoy expression ta lekha hoy na
+
+
+// this value syntax 
+// let user ={
+//     age:40,
+//     getAge : function () {
+//         console.log("Normal function",this.age)
+//     },
+//     getAgeDetails: ()=>{
+//        console.log("Arrow Function", this.age)
+//     }
+// }
+// console.log(user.age)
+// console.log(user.getAge())
+// console.log(user.getAgeDetails())
+
+// arrow function er khettre this keyword kaj kore na 
+
+// access before declaring
+
+// const result = add(5,4)
+// console.log(result)
+// function add(num1,num2){
+//     return num1 + num2
+// }
