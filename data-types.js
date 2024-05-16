8a13ce23b438b702eed20ebbfb196ee027101dd8
BappySheikh1 <sheikhbappy91@gmail.com>
2024-05-16 09:31:19 +0600

    first commit

---
+++ data-types.js
@@ -0,0 +1,17 @@
+// // Datatypes
+// // Primitive : String, Number, Boolean, Null, undefined
+// // Non-Primitive : Refrence Variables (Object, Array, Function)
+
+// let a = 5;
+// let b =a
+// b=7
+
+// console.log('a',a)
+// console.log('b',b)
+
+// let nums1 = [1,2,3]
+// let nums2 = nums1
+
+// nums2.push(5,5)
+// console.log(nums1)
+// console.log(nums2)
---
+++ function.js
@@ -0,0 +1,19 @@
+// normal function
+
+// function add(num1, num2){
+//   return num1 + num2
+// }
+
+// const result = add(5, 7)
+
+// console.log(result)
+
+// Arrow function
+
+// const addNumbers = (num1, num2) =>{
+//   return num1 + num2
+// }
+
+// const result = addNumbers(10,22)
+// console.log(result)
+
